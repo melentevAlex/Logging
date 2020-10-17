ff0d1d8c367288bd6ce21fd9587e6a2a069357df
melentevAlex <melentAlex@yandex.ru>
2020-10-17 23:41:39 +0300

    first commit

---
+++ ExLogg/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ExLogg.Models;
+using NLog;
+
+namespace ExLogg.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetStudent(int id)
+        {
+            logger.Info("Преподаватель запросил студента с id == " + id);
+            StudentsRepository repository = new StudentsRepository();
+
+            Student student = repository.GetStudentById(id);
+
+            return View(student);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ExLogg/Models/Students.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExLogg.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class StudentsRepository
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        //записи находятся в базе данных
+        private readonly List<Student> _studentsList = new List<Student>()
+        {
+            new Student() { Id = 1, Name = "George", Age = 19 },
+            new Student() { Id = 2, Name = "John", Age = 22 },
+            new Student() { Id = 3, Name = "Mark", Age = 14 }
+        };
+
+        public List<Student> GetStudents()
+        {
+            logger.Trace("Произведено подключение к базе данных.");
+            logger.Trace("Произведена выборка всех студентов");
+            return _studentsList;
+        }
+
+        public Student GetStudentById(int id)
+        {
+            logger.Trace("Запрашиваемый id студента: " + id);
+            logger.Trace("Попытка подключения к источнику данных");
+            logger.Trace("Подключение прошло успешно. Затраченное время (мс): " + new TimeSpan(0, 0, 0, 0, 20).Milliseconds);
+            var student = _studentsList.FirstOrDefault(x => x.Id == id);
+            return student;
+        }
+    }
+}
